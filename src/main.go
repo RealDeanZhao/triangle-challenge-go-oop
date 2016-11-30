e6aeaa2677f127f3842ecafb15e77d0db7cd9104
Dean Zhao <zestxjest@outlook.com>
2016-11-30 22:54:21 +0800

    refactoring some code

--- src/main.go
+++ src/main.go
@@ -8,26 +8,6 @@ import (
 	libs "./libs"
 )
 
-func convertUserInput(a string, b string, c string) libs.UserInput {
-	floatA, err := strconv.ParseFloat(a, 64)
-	if err != nil {
-		panic("The side A cannot be converted to float64")
-	}
-	floatB, err := strconv.ParseFloat(b, 64)
-	if err != nil {
-		panic("The side B cannot be converted to float64")
-	}
-	floatC, err := strconv.ParseFloat(c, 64)
-	if err != nil {
-		panic("The side C cannot be converted to float64")
-	}
-	return libs.UserInput{
-		SideA: floatA,
-		SideB: floatB,
-		SideC: floatC,
-	}
-}
-
 func main() {
 	input := convertUserInput(os.Args[1], os.Args[2], os.Args[3])
 	validator := libs.TriangleValidator{
@@ -54,6 +34,26 @@ func main() {
 		isScalene := checkType(&scaleneChecker)
 		fmt.Println("Is it a Scalene?:", isScalene)
 	}
+}
+
+func convertUserInput(a string, b string, c string) libs.UserInput {
+	floatA := convertOneSide(a)
+	floatB := convertOneSide(b)
+	floatC := convertOneSide(c)
+
+	return libs.UserInput{
+		SideA: floatA,
+		SideB: floatB,
+		SideC: floatC,
+	}
+}
+
+func convertOneSide(side string) float64 {
+	floatSide, err := strconv.ParseFloat(side, 64)
+	if err != nil {
+		panic("The side A cannot be converted to float64")
+	}
+	return floatSide
 }
 
 func checkType(checker libs.ITypeChecker) bool {
